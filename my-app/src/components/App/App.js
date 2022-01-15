20ba30801f6462f1f62e8796fea8c358d641f068
JustinPong <Justinpong0416@gmail.com>
2022-01-15 20:07:55 +0800

    addTask worked

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -1,18 +1,29 @@
-import TaskManager from'../TaskManager/TaskManger';
+import TaskManager from '../TaskManager/TaskManger';
 import TaskList from '../../components/TaskList/TaskList';
-import Card from '../../components/Card/Card';
+import Item from '../../components/Item/Item';
 import React from 'react';
 
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+  };
+  }
 
+  addTodo = (todo) => {
+      this.setState({
+          todos: [todo, ...this.state.todos]
+      });
+  }
+
+  render() {
     return (
       <div className="container">
         <h1 className="title">Task List</h1>
-        <TaskManager />
-        <TaskList />
-        {/* <Card /> */}
+        <TaskManager onSubmit={this.addTodo} />
+        <TaskList todos={this.state.todos} onDelete={this.handleChangeDelete} />
       </div>
     );
   }
