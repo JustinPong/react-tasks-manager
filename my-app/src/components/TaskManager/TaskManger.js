e327101a807ca4a36c82468b7f047292c5c398e1
JustinPong <Justinpong0416@gmail.com>
2022-01-23 23:44:47 +0800

    added error message

--- my-app/src/components/TaskManager/TaskManger.js
+++ my-app/src/components/TaskManager/TaskManger.js
@@ -26,7 +26,6 @@ class TaskManager extends React.Component {
         this.setState({
             [event.target.name]: event.target.value
         })
-        console.log(typeof(this.state.date))
     }
 
     handleSubmit = (event) => {
@@ -82,17 +81,25 @@ class TaskManager extends React.Component {
                 <h2 className="new-task-title">New Task</h2>
                 <form onSubmit={this.handleSubmit} id="new-task-form">
                     <div className="form-group">
+                        <div className="flex">
                         <label htmlFor="name-input">Name</label>
+                        <div className="errorMessage">{this.state.nameError ? '(Please enter a task name)' : ''}</div>
+                        </div>
                         <input style={{ border: this.state.nameError ? '2px red solid' : '' }} name="name" value={this.state.name} onChange={this.handleChange} className="col-12" id="name-input" />
-
                     </div>
                     <div className="form-group">
+                        <div className="flex">
                         <label htmlFor="description-input">Description</label>
+                        <div className="errorMessage">{this.state.descriptionError ? '(Please enter a description)' : ''}</div>
+                        </div>
                         <textarea style={{ border: this.state.descriptionError ? '2px red solid' : '' }} name="description" value={this.state.description} onChange={this.handleChange} className="col-12" type="text"></textarea>
                     </div>
                     <div className="row-form row">
                         <div className="form-group2 col-6">
+                            <div className="flex">
                             <label htmlFor="assignedTo-input">Assigned To</label>
+                            <div className="errorMessage">{this.state.assignedToError ? '(Please enter a assigned to)' : ''}</div>
+                            </div>
                             <input style={{ border: this.state.assignedToError ? '2px red solid' : '' }} name="assignedTo" value={this.state.assignedTo} onChange={this.handleChange} className="col-12" id="assignedTo-input" />
                         </div>
                         <div className="form-group2 col-6">
