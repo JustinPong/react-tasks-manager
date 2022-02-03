2c1ef9a3256fc55d2efddd1f456f5d88d6daf471
JoLam <jolampersonal@gmail.com>
2022-02-03 23:41:24 +0800

    after-fix-bug

--- my-app/src/components/TaskManager/TaskManger.js
+++ my-app/src/components/TaskManager/TaskManger.js
@@ -14,7 +14,7 @@ async function fetchTask(setTask) {
 
 function TaskManager(props) {
 
-    const [task, setTask] = useState({
+    const [task, setTask] = useState ({
         id: shortid.generate(),
         name: '',
         description: '',
@@ -22,8 +22,7 @@ function TaskManager(props) {
         date: '',
         isChecked: false,
         nameError: false,
-        descriptionError: false,
-        assignedToError: false
+        descriptionError: false
     })
 
     useEffect(() => {
@@ -52,32 +51,17 @@ function TaskManager(props) {
     const handleSubmit = (event) => {
         if (task.name === '' || task.description === '' || task.assignedTo === '') {
             if (task.name === '') {
-                setTask({nameError: true})
+                setTask(prevTask => ({...prevTask, nameError: true}))
             } else {
-                setTask({nameError: false})
+                setTask(prevTask => ({...prevTask, nameError: false}))
             }
             if (task.description === '') {
-               setTask({descriptionError: true})
+               setTask(prevTask => ({...prevTask, descriptionError: true}))
             } else {
-                setTask({descriptionError: false})
+                setTask(prevTask => ({...prevTask, descriptionError: false}))
             }
-            if (task.assignedTo === '') {
-                setTask({assignedToError: true})
-            } else {
-                setTask({assignedToError: false})
-            }
-        } else {
-            props.onSubmit({
-                id: shortid.generate(),
-                name: task.name,
-                description: task.description,
-                assignedTo: task.assignedTo,
-                date: task.date,
-                isChecked: false,
-                nameError: false,
-                descriptionError: false,
-                assignedToError: false
-            })
+        } else if(task.name && task.description && task.assignedTo) {
+             props.onSubmit(task)
                setTask({
                     id: "",
                     name: "",
@@ -85,8 +69,7 @@ function TaskManager(props) {
                     assignedTo: "",
                     date: "",
                     nameError: false,
-                    descriptionError: false,
-                    assignedToError: false
+                    descriptionError: false
                 });
         }
     }
@@ -96,16 +79,18 @@ function TaskManager(props) {
             <form onSubmit={handleSubmit} id="new-task-form">
                 <div className="form-group">
                     <label htmlFor="name-input">Name</label>
-                    <input style={{ border: useLocalStorage.nameError ? '2px red solid' : '' }} name="name" value={task.name} onChange={handleChange} className="col-12" id="name-input" />
+                    <div className="errorMessage">{task.nameError ? '(Please enter a task name)' : ''}</div>
+                    <input style={{ border: task.nameError ? '2px red solid' : '' }} name="name" value={task.name} onChange={handleChange} className="col-12" id="name-input" required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="description-input1">Description</label>
-                    <textarea style={{ border: useLocalStorage.descriptionError ? '2px red solid' : '' }} name="description" value={task.description} onChange={handleChange} className="col-12" type="text"></textarea>
+                    <div className="errorMessage">{task.descriptionError ? '(Please enter a description)' : ''}</div>
+                    <textarea style={{ border: task.descriptionError ? '2px red solid' : '' }} name="description" value={task.description} onChange={handleChange} className="col-12" type="text"></textarea>
                 </div>
                 <div className="row-form row">
                     <div className="form-group2 col-6">
                         <label htmlFor="assignedTo-input">Assigned To</label>
-                        <input style={{ border: useLocalStorage.assignedToError ? '2px red solid' : '' }} name="assignedTo" value={task.assignedTo} onChange={handleChange} className="col-12" id="assignedTo-input" />
+                        <input name="assignedTo" value={task.assignedTo} onChange={handleChange} className="col-12" id="assignedTo-input" />
                     </div>
                     <div className="form-group2 col-6">
                         <label htmlFor="date-input">Date</label>
@@ -118,7 +103,6 @@ function TaskManager(props) {
             </form>
         </div>
     )
-
 }
 
 export default TaskManager;
